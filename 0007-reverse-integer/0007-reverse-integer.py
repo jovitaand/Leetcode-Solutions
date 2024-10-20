e40821223440b5620059ae830d874919d035323c
Jovita Andrews <52595708+jovitaand@users.noreply.github.com>
2024-10-20 16:33:23 -0400

    Time: 34 ms (5.5%), Space: 13.3 MB (7.4%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,3 +1,35 @@
 class Solution:
-    def reverse(self, x: int) -> int:
+    def reverse(self, x):
+        MAX = 2**31-1
+        MIN = -2**31
+        flag = False
         
+        'convert int to str'
+        char = str(x)
+        
+        'if it is negative rise a flag'
+        if char[0]=='-':
+            char = char[1:] #start after the negative sign
+            flag = True
+            
+        'reverse a string'
+        rev=char[::-1]
+        
+        'putting back the minus sign'
+        if flag == True:
+            rev = '-'+ rev
+        
+        'convert back string into int'
+        rev= int(rev)
+        
+        'checking out of bounds conditions'
+        if rev > MAX or rev < MIN: rev=0
+        return rev
+        
+        
+ 
+ 
+
+         
+         
+      
