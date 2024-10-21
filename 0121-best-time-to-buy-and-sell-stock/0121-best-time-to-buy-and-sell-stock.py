9e3a9603583baa0658a1706110bc5288a2b6d033
Jovita Andrews <52595708+jovitaand@users.noreply.github.com>
2024-10-21 11:25:26 -0400

    Time: 66 ms (99.27%), Space: 26 MB (98.84%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,9 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        profit = 0
-        start = 0
-        for i in range(1,len(prices)):
-            if prices[i]<prices[start]:
-                start = i
-            else:
-                profit = max(profit,prices[i]-prices[start])
-        return profit
-    #using 2 pointers to identify the best time to buy and sell
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        lowest = prices[0]
+        for price in prices:
+            if price < lowest:
+                lowest = price
+            res = max(res, price - lowest)
+        return res
