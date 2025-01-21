81815362d328fd44baaf3769bb99da6561b6dc74
Jovita Andrews <52595708+jovitaand@users.noreply.github.com>
2025-01-21 15:22:31 -0500

    Time: 407 ms (56.07%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,3 +1,4 @@
-SELECT name, population, area
-FROM World
-WHERE area>= '3000000' || population >= '25000000';
+# Write your MySQL query statement below
+select name, population, area
+from World
+where area>= '3000000' || population >= '25000000';
