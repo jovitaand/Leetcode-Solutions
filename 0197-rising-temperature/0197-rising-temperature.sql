973989b261af60eee16a666ac0960e0329b4c844
Jovita Andrews <52595708+jovitaand@users.noreply.github.com>
2025-01-21 21:48:27 -0500

    Time: 1298 ms (9.1%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,5 @@
-select w1.id 
-from weather w1
-join weather w2
-on w1.temperature>w2.temperature and datediff( w1.recordDate, w2.recordDate) =1;
-
+# Write your MySQL query statement below
+SELECT today.id
+FROM Weather yesterday
+JOIN Weather today
+ON today.temperature > yesterday.temperature AND datediff( today.recordDate, yesterday.recordDate) = 1;
