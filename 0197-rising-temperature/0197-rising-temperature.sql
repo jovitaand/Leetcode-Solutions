7c850a54f7f054f23d9f76108267c05623a7ab10
Jovita Andrews <52595708+jovitaand@users.noreply.github.com>
2025-01-21 21:49:09 -0500

    Time: 701 ms (51.03%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,3 +3,4 @@ SELECT today.id
 FROM Weather yesterday
 JOIN Weather today
 ON today.temperature > yesterday.temperature AND datediff( today.recordDate, yesterday.recordDate) = 1;
+#datediff(): returns the difference between 2 dates as an integer.
