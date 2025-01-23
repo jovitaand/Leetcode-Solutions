948cdae2386d44a73271dced2486f2746d60d6c4
Jovita Andrews <52595708+jovitaand@users.noreply.github.com>
2025-01-22 21:21:21 -0500

    Time: 1657 ms (19.6%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -6,3 +6,5 @@ left join UnitsSold as u
 on p.product_id = u.product_id  AND
 u.purchase_date BETWEEN p.start_date and p.end_date
 group by p.product_id;
+/* average selling price = total revenue/number of units sold
+the purchase date should be between the start and the end date */
