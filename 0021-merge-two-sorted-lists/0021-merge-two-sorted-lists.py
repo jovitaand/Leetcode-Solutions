bc9e522a0c0de874277a94d4fbd5c108d8f6cc3a
Jovita Andrews <52595708+jovitaand@users.noreply.github.com>
2025-01-31 14:00:30 -0500

    Time: 0 ms (100%), Space: 17.7 MB (47.64%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -10,8 +10,10 @@ class Solution:
         if list2 is None:
             return list1
         if list1.val <= list2.val:
+            #If list1.val is smaller, attach it to the recursive result of list1.next and list2.
             list1.next = self.mergeTwoLists(list1.next, list2)
             return list1
         else:
+            #If list2.val is smaller, attach it to the recursive result of list1 and list2.next.
             list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
